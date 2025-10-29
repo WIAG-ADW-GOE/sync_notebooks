1573b9b8b663ae4dd4fc88b1fc2cb91a7b586cb2
Felix Schwart <util.fs@tuta.com>
2025-10-29 13:12:07 +0100

    only query needed columns

--- queries/get_wiag_roles.sql
+++ queries/get_wiag_roles.sql
@@ -1,4 +1,4 @@
-SELECT * FROM role r 
+SELECT r.id, r.name, r.factgrid_id FROM role as r 
 LEFT JOIN (
     select * from url_external where authority_id = 42
 ) u on r.id = u.item_id ORDER BY r.id;
